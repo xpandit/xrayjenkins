8c3e07f297b86f036dc3f8b9b6d8b6da75a16a3f
Xavier Fernandes <xcfe@XPLAPTOP107.XPAND>
2018-07-02 16:44:10 +0100

    XRAYJENKINS-5

--- src/test/java/com/xpandit/plugins/xrayjenkins/Utils/FileUtilsTest.java
+++ src/test/java/com/xpandit/plugins/xrayjenkins/Utils/FileUtilsTest.java
@@ -11,6 +11,7 @@ import hudson.FilePath;
 import hudson.model.*;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 import org.junit.*;
 import org.junit.rules.TemporaryFolder;
@@ -28,30 +29,40 @@ public class FileUtilsTest {
     @Mock
     private TaskListener taskListener;
 
+    private static final String XRAYJENKINS = "xrayjenkins";
+    private static final String WORK = "work";
+    private static final String WORKSPACEFOLDER = "workspace";
+    private static final String DUMMYPROJECT = "dummyproject";
+    private static final String UNITTESTING = "unittesting";
+    private static final String RESULTS = "results";
+    private static final String FOLDERMATCHER = "**";
+    private static final String EXCEPTION_MESSAGE = "An exception occurred when performing the Test: ";
+    private static final String LOGGER_NAME = "log.txt";
+
     private void prepareFolders() throws IOException{
-        File fa = workspace.newFolder("xrayjenkins","work", "workspace", "dummyproject", "joaquina");
+        File fa = workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER, DUMMYPROJECT, UNITTESTING);
         File.createTempFile("potatoe", ".txt", fa).deleteOnExit();
         File.createTempFile("hello", ".xml", fa).deleteOnExit();
-        File fa1b = workspace.newFolder("xrayjenkins","work", "workspace", "dummyproject","joaquina","a1","results");
+        File fa1b = workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER, DUMMYPROJECT,UNITTESTING,"a1",RESULTS);
         File.createTempFile("antonio", ".jpg", fa1b).deleteOnExit();
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a1","results","b1");
-        File fa1b2 = workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a1","results","b2");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a1",RESULTS,"b1");
+        File fa1b2 = workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a1",RESULTS,"b2");
         File.createTempFile("vacations", ".jpg", fa1b2).deleteOnExit();
         File.createTempFile("result_fa1b2_1", ".xml", fa1b2).deleteOnExit();//this is a result file
         File.createTempFile("result_fa1b2_3", ".xml", fa1b2).deleteOnExit();//this is a result file
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a1","results","b3");
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a2","results","b1");
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a2","results","b2");
-        File fa2b3 = workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a2","results","b3");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a1",RESULTS,"b3");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a2",RESULTS,"b1");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a2",RESULTS,"b2");
+        File fa2b3 = workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a2",RESULTS,"b3");
         File.createTempFile("result_fa2b3_1", ".xml", fa2b3).deleteOnExit();//this is a result file
         File.createTempFile("result_fa2b3_2", ".xml", fa2b3).deleteOnExit();//this is a result file
         File.createTempFile("rockyII", ".avi", fa2b3).deleteOnExit();
-        File fa3b = workspace.newFolder( "xrayjenkins","work", "workspace", "dummyproject", "joaquina","a3","results");
+        File fa3b = workspace.newFolder( XRAYJENKINS,WORK, WORKSPACEFOLDER, DUMMYPROJECT, UNITTESTING,"a3",RESULTS);
         File.createTempFile("movie1", ".zip", fa3b).deleteOnExit();
         File.createTempFile("backup",".xml", fa3b).deleteOnExit();
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a3","results","b1");
-        workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a3","results","b2");
-        File fa3b3 = workspace.newFolder("xrayjenkins","work", "workspace","dummyproject", "joaquina","a3","results","b3");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a3",RESULTS,"b1");
+        workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a3",RESULTS,"b2");
+        File fa3b3 = workspace.newFolder(XRAYJENKINS,WORK, WORKSPACEFOLDER,DUMMYPROJECT, UNITTESTING,"a3",RESULTS,"b3");
         File.createTempFile("result_fa3b3_1",".xml", fa3b3).deleteOnExit(); //this is a result file
         File.createTempFile("february_05_fa3b3",".xml", fa3b3).deleteOnExit(); //this is a result file
         File.createTempFile("february_17_fa3b3",".xml", fa3b3).deleteOnExit(); //this is a result file
@@ -60,17 +71,15 @@ public class FileUtilsTest {
     @Test
     public void testGetFileWithAbsolutePath(){
         try{
-            when(taskListener.getLogger()).thenReturn(System.out);
+            try(PrintStream logger = new PrintStream(workspace.newFile(LOGGER_NAME))){
+                when(taskListener.getLogger()).thenReturn(logger);
+            }
             prepareFolders();
-            String basePath = workspace.getRoot().toPath().toString();
-            File workspaceFile = new File(basePath + getWorkSpaceSuffix());
-            String resultsPath = basePath + getAbsoluteDirectoryPath() + "*.xml";
-
-            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(workspaceFile), resultsPath, taskListener);
+            String resultsPath = workspace.getRoot().toPath().toString() + getAbsoluteDirectoryPath() + "*.xml";
+            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(getWorkspaceFile()), resultsPath, taskListener);
             Assert.assertTrue(matchingFiles.size() == 7);
-
         } catch (IOException e){
-            Assert.fail("An exception occured when performing the Test: " + e.getMessage());
+            Assert.fail(EXCEPTION_MESSAGE + e.getMessage());
         }
         workspace.delete();
     }
@@ -78,72 +87,68 @@ public class FileUtilsTest {
     @Test
     public void testGetFileWithRelativePath(){
         try{
-            when(taskListener.getLogger()).thenReturn(System.out);
+            try(PrintStream logger = new PrintStream(workspace.newFile(LOGGER_NAME))){
+                when(taskListener.getLogger()).thenReturn(logger);
+            }
             prepareFolders();
-            String basePath = workspace.getRoot().toPath().toString();
-            File workspaceFile = new File(basePath + getWorkSpaceSuffix());
-            String resultsPath = "\\joaquina\\**\\results\\**\\*.xml";
-            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(workspaceFile), resultsPath, taskListener);
+            String resultsPath = "\\unittesting\\**\\results\\**\\*.xml";
+            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(getWorkspaceFile()), resultsPath, taskListener);
             Assert.assertTrue(matchingFiles.size() == 7);
-
         } catch (IOException e){
-            Assert.fail("An exception occured when performing the Test: " + e.getMessage());
+            Assert.fail(EXCEPTION_MESSAGE + e.getMessage());
         }
         workspace.delete();
     }
 
+    private File getWorkspaceFile(){
+        return new File(workspace.getRoot().toPath().toString() + getWorkSpaceSuffix());
+    }
+
     private String getWorkSpaceSuffix(){
-        StringBuilder sb = new StringBuilder();
-        return sb.append(File.separator)
-                .append("xrayjenkins")
-                .append(File.separator)
-                .append("work")
-                .append(File.separator)
-                .append("workspace")
-                .append(File.separator)
-                .append("dummyproject")
-                .toString();
+        return File.separator
+                + XRAYJENKINS
+                + File.separator
+                + WORK
+                + File.separator
+                + WORKSPACEFOLDER
+                + File.separator
+                + DUMMYPROJECT;
     }
 
     @Test
     public void testGetFileWithSofisticatedGlobExpression(){
         try{
-            when(taskListener.getLogger()).thenReturn(System.out);
+            try(PrintStream logger = new PrintStream(workspace.newFile(LOGGER_NAME))){
+                when(taskListener.getLogger()).thenReturn(logger);
+            }
             prepareFolders();
-            String basePath = workspace.getRoot().toPath().toString();
-            File workspaceFile = new File(basePath + getWorkSpaceSuffix());
-            String resultsPath = basePath + getAbsoluteDirectoryPath() + "feb*.xml";
-
-
-            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(workspaceFile), resultsPath, taskListener);
+            String resultsPath = workspace.getRoot().toPath().toString() + getAbsoluteDirectoryPath() + "feb*.xml";
+            List<FilePath> matchingFiles = FileUtils.getFilePaths(new FilePath(getWorkspaceFile()), resultsPath, taskListener);
             Assert.assertTrue(matchingFiles.size() == 2);
-
         } catch (IOException e){
-            Assert.fail("An exception occured when performing the Test: " + e.getMessage());
+            Assert.fail(EXCEPTION_MESSAGE + e.getMessage());
         }
         workspace.delete();
     }
 
     private String getAbsoluteDirectoryPath(){
-        StringBuilder sb = new StringBuilder();
-        return sb.append(File.separator)
-                .append("xrayjenkins")
-                .append(File.separator)
-                .append("work")
-                .append(File.separator)
-                .append("workspace")
-                .append(File.separator)
-                .append("dummyproject")
-                .append(File.separator)
-                .append("joaquina")
-                .append(File.separator)
-                .append("**")
-                .append(File.separator)
-                .append("results")
-                .append(File.separator)
-                .append("**")
-                .append(File.separator)
-                .toString();
+        return  File.separator
+                + XRAYJENKINS
+                + File.separator
+                + WORK
+                + File.separator
+                + WORKSPACEFOLDER
+                + File.separator
+                + DUMMYPROJECT
+                + File.separator
+                + UNITTESTING
+                + File.separator
+                + FOLDERMATCHER
+                + File.separator
+                + RESULTS
+                + File.separator
+                + FOLDERMATCHER
+                + File.separator;
     }
 
 }
